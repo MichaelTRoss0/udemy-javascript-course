b7dfdb5af6ac8a7ca2dad44408d63789d79c798b
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-02-02 13:36:13 -0500

    Initial commit; this does include work from the first section of lessons

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,45 @@
+// const country = 'United States';
+// const continent = 'North America';
+// let population = 332; // in millions
+// // let population = 130;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// const isIsland = false;
+// let language;
+// console.log(isIsland);
+// console.log(language);
+
+// language = 'English';
+// // isIsland = true;
+
+// let halvedPopulation = population / 2;
+// population += 1;
+// console.log(population);
+// console.log(population > 6);
+// console.log(population < 33);
+// const description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
+// console.log(description);
+
+// const descriptionNew = `${country} is in ${continent}, and its ${population} million people speak ${language}.`;
+// console.log(descriptionNew);
+
+// if (population > 33) {
+//     console.log(`${country}'s population is above average.`);
+// } else {
+//     console.log(`${country}'s population is ${33 - population} million below average.`);
+// }
+
+// '9' - '5';                  // '4'      4
+// '19' - '13' + '17';         // '617'    '617'
+// '19' - '13' + 17;           // '617'    23
+// '123' < 57;                 // false    false
+// 5 + 6 + '4' + 9 - 4 - 2;    // '1043'   1143
+
+// console.log('9' - '5');
+// console.log('19' - '13' + '17');
+// console.log('19' - '13' + 17);
+// console.log('123' < 57);
+// console.log(5 + 6 + '4' + 9 - 4 - 2);
+
---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,280 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javaScriptIsFun = 'YES';
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Ross';
+console.log(lastName);
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4 // x = x * 4 = 100
+x++ // x = + 1 = 101
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge);
+*/
+
+/////////////////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+GOOD LUCK �
+
+const markMass1 = 78;
+const markHeight1 = 1.69;
+const johnMass1 = 92;
+const johnHeight1 = 1.95;
+
+const markMass2 = 95;
+const markHeight2 = 1.88;
+const johnMass2 = 85;
+const johnHeight2 = 1.76;
+
+const markBMI1 = markMass1 / markHeight1 ** 2;
+const johnBMI1 = johnMass1 / johnHeight1 ** 2;
+console.log(markBMI1, johnBMI1);
+
+const markBMI2 = markMass2 / markHeight2 ** 2;
+const johnBMI2 = johnMass2 / johnHeight2 ** 2;
+console.log(markBMI2, johnBMI2);
+
+const markHigherBMI1 = markBMI1 > johnBMI1;
+const markHigherBMI2 = markBMI2 > johnBMI2;
+console.log(markHigherBMI1, markHigherBMI2);
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`)
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String
+multiple
+lines`);
+
+const age = 15;
+
+if (age >= 18) {
+    console.log('Sarah can start her driving license 🚗');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 2012;
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+*/
+
+/////////////////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Use the BMI example from Challenge #1, and the code you already wrote, and
+improve it.
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!"
+Hint: Use an if/else statement �
+GOOD LUCK �
+
+const markMass1 = 78;
+const markHeight1 = 1.69;
+const johnMass1 = 92;
+const johnHeight1 = 1.95;
+
+const markMass2 = 95;
+const markHeight2 = 1.88;
+const johnMass2 = 85;
+const johnHeight2 = 1.76;
+
+const markBMI1 = markMass1 / markHeight1 ** 2;
+const johnBMI1 = johnMass1 / johnHeight1 ** 2;
+if (markBMI1 > johnBMI1) {
+    console.log(`In the first test, Mark's BMI (${markBMI1}) is higher than John's (${johnBMI1})!`);
+} else {
+    console.log(`In the first test, John's BMI (${johnBMI1}) is higher than Mark's (${markBMI1})!`);
+}
+
+const markBMI2 = markMass2 / markHeight2 ** 2;
+const johnBMI2 = johnMass2 / johnHeight2 ** 2;
+if (markBMI2 > johnBMI2) {
+    console.log(`In the second test, Mark's BMI (${markBMI2}) is higher than John's (${johnBMI2})!`);
+} else {
+    console.log(`In the second test, John's BMI (${johnBMI2}) is higher than Mark's (${markBMI2})!`);
+}
+
+// Type Conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// Type Coercion
+console.log('I am ' + 23 + ' years old');
+console.log('I am ' + '23' + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' * '2');
+console.log('23' > '18');
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
+*/
+
+// Five falsy values: 0, '', undefined, null, NaN
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

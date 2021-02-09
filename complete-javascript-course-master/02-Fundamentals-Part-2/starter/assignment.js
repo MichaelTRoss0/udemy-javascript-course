2fde95ec837c564b4e559781ff5f7ee4ca55b215
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-02-09 14:43:47 -0500

    Completed the lesson "Reviewing Functions"

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignment.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignment.js
@@ -1,39 +1,40 @@
-'use strict';
+// 'use strict';
 
-function describeCountry(country, population, capitalCity) {
-    return `${country} has ${population} million people and its capital city is ${capitalCity}`;
-}
-const unitedStates = describeCountry('United States', 328, 'Washington D.C.');
-const canada = describeCountry('Canada', 38, 'Ottowa');
-const mexico = describeCountry('Mexico', 126, 'Mexico City');
-console.log(unitedStates);
-console.log(canada);
-console.log(mexico);
+// function describeCountry(country, population, capitalCity) {
+//     return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+// }
+// const unitedStates = describeCountry('United States', 328, 'Washington D.C.');
+// const canada = describeCountry('Canada', 38, 'Ottowa');
+// const mexico = describeCountry('Mexico', 126, 'Mexico City');
+// console.log(unitedStates);
+// console.log(canada);
+// console.log(mexico);
 
-function percentageOfWorld1(population) {
-    return population / 7900 * 100;
-}
-const percentageUS = percentageOfWorld1(328);
-const percentageCN = percentageOfWorld1(38);
-const percentageMX = percentageOfWorld1(126);
-console.log(percentageUS);
-console.log(percentageCN);
-console.log(percentageMX);
-const percentageOfWorld2 = function (population) {
-    return population / 7900 * 100;
-}
-console.log(percentageOfWorld2(328));
-console.log(percentageOfWorld2(38));
-console.log(percentageOfWorld2(126));
+// function percentageOfWorld1(population) {
+//     return population / 7900 * 100;
+// }
+// const percentageUS = percentageOfWorld1(328);
+// const percentageCN = percentageOfWorld1(38);
+// const percentageMX = percentageOfWorld1(126);
+// console.log(percentageUS);
+// console.log(percentageCN);
+// console.log(percentageMX);
+// const percentageOfWorld2 = function (population) {
+//     return population / 7900 * 100;
+// }
+// console.log(percentageOfWorld2(328));
+// console.log(percentageOfWorld2(38));
+// console.log(percentageOfWorld2(126));
 
-const percentageOfWorld3 = population => population / 7900 * 100;
-console.log(percentageOfWorld3(328));
-console.log(percentageOfWorld3(38));
-console.log(percentageOfWorld3(126));
+// const percentageOfWorld3 = population => population / 7900 * 100;
+// console.log(percentageOfWorld3(328));
+// console.log(percentageOfWorld3(38));
+// console.log(percentageOfWorld3(126));
 
-function describePopulation(country, population) {
-    return `${country} has a population of ${population} million people, which is about ${percentageOfWorld1(population)}% of the world.`;
-}
-console.log(describePopulation('United States', 328));
-console.log(describePopulation('Canada', 38));
-console.log(describePopulation('Mexico', 126));
+// function describePopulation(country, population) {
+//     return `${country} has a population of ${population} million people, which is about ${percentageOfWorld1(population)}% of the world.`;
+// }
+// console.log(describePopulation('United States', 328));
+// console.log(describePopulation('Canada', 38));
+// console.log(describePopulation('Mexico', 126));
+
--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -64,7 +64,7 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1980, 'Bob'));
-*/
+
 
 function cutFruitPieces(fruit) {
     return fruit * 3;
@@ -78,3 +78,23 @@ function fruitProcessor(apples, oranges) {
     return juice;
 }
 console.log(fruitProcessor(2, 3));
+*/
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired 🎉`);
+        return -1;
+    }
+}
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));

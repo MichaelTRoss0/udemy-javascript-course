f50e1310809884e7fd1b58e8b0651654b4312edd
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-02-17 14:16:44 -0500

    Completed Coding Challenge #2 for JavaScript Fundamentals - Part 2

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -156,7 +156,7 @@ console.log(age1, age2, age3);
 
 const ages = [calcAge1(years[0]), calcAge1(years[1]), calcAge1(years[years.length - 1])];
 console.log(ages);
-*/
+
 
 const friends = ['Michael', 'Steven', 'Peter'];
 
@@ -188,3 +188,16 @@ console.log(friends.includes(23));
 if (friends.includes('Steven')) {
     console.log('You have a friend called Steven');
 }
+*/
+
+// Coding Challenge #2
+function calcTip(bill) {
+    return 50 <= bill && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+console.log(calcTip(100));
+const bills = [125, 555, 44];
+console.log(bills);
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total);

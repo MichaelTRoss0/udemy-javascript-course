fd12a913e291c3b52e78c7cc4d0d07fd38759e2a
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-02-18 13:20:19 -0500

    Completed the lesson "Introduction to Objects"

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignment.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignment.js
@@ -1,53 +1,61 @@
-// 'use strict';
+'use strict';
 
-// function describeCountry(country, population, capitalCity) {
-//     return `${country} has ${population} million people and its capital city is ${capitalCity}`;
-// }
-// const unitedStates = describeCountry('United States', 328, 'Washington D.C.');
-// const canada = describeCountry('Canada', 38, 'Ottowa');
-// const mexico = describeCountry('Mexico', 126, 'Mexico City');
-// console.log(unitedStates);
-// console.log(canada);
-// console.log(mexico);
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+}
+const unitedStates = describeCountry('United States', 328, 'Washington D.C.');
+const canada = describeCountry('Canada', 38, 'Ottowa');
+const mexico = describeCountry('Mexico', 126, 'Mexico City');
+console.log(unitedStates);
+console.log(canada);
+console.log(mexico);
 
-// function percentageOfWorld1(population) {
-//     return population / 7900 * 100;
-// }
-// const percentageUS = percentageOfWorld1(328);
-// const percentageCN = percentageOfWorld1(38);
-// const percentageMX = percentageOfWorld1(126);
-// console.log(percentageUS);
-// console.log(percentageCN);
-// console.log(percentageMX);
-// const percentageOfWorld2 = function (population) {
-//     return population / 7900 * 100;
-// }
-// console.log(percentageOfWorld2(328));
-// console.log(percentageOfWorld2(38));
-// console.log(percentageOfWorld2(126));
+function percentageOfWorld1(population) {
+    return population / 7900 * 100;
+}
+const percentageUS = percentageOfWorld1(328);
+const percentageCN = percentageOfWorld1(38);
+const percentageMX = percentageOfWorld1(126);
+console.log(percentageUS);
+console.log(percentageCN);
+console.log(percentageMX);
+const percentageOfWorld2 = function (population) {
+    return population / 7900 * 100;
+}
+console.log(percentageOfWorld2(328));
+console.log(percentageOfWorld2(38));
+console.log(percentageOfWorld2(126));
 
-// const percentageOfWorld3 = population => population / 7900 * 100;
-// console.log(percentageOfWorld3(328));
-// console.log(percentageOfWorld3(38));
-// console.log(percentageOfWorld3(126));
+const percentageOfWorld3 = population => population / 7900 * 100;
+console.log(percentageOfWorld3(328));
+console.log(percentageOfWorld3(38));
+console.log(percentageOfWorld3(126));
 
-// function describePopulation(country, population) {
-//     return `${country} has a population of ${population} million people, which is about ${percentageOfWorld1(population)}% of the world.`;
-// }
-// console.log(describePopulation('United States', 328));
-// console.log(describePopulation('Canada', 38));
-// console.log(describePopulation('Mexico', 126));
+function describePopulation(country, population) {
+    return `${country} has a population of ${population} million people, which is about ${percentageOfWorld1(population)}% of the world.`;
+}
+console.log(describePopulation('United States', 328));
+console.log(describePopulation('Canada', 38));
+console.log(describePopulation('Mexico', 126));
 
-// const populations = [328, 38, 126, 56]; // United States, Canada, Mexico, England
-// console.log(populations.length === 4);
-// const percentages = [percentageOfWorld1(328), percentageOfWorld1(38), percentageOfWorld1(126), percentageOfWorld1(56)];
-// console.log(percentages);
+const populations = [328, 38, 126, 56]; // United States, Canada, Mexico, England
+console.log(populations.length === 4);
+const percentages = [percentageOfWorld1(328), percentageOfWorld1(38), percentageOfWorld1(126), percentageOfWorld1(56)];
+console.log(percentages);
 
-// var neighbors = ['Canada', 'Mexico'];
-// neighbors.push('Utopia');
-// neighbors.pop();
-// if (!neighbors.includes('Germany')) {
-//     console.log('Probably not a central European country :D')
-// }
-// neighbors[0] = 'Parliamentary Democracy of Canada';
+var neighbors = ['Canada', 'Mexico'];
+neighbors.push('Utopia');
+neighbors.pop();
+if (!neighbors.includes('Germany')) {
+    console.log('Probably not a central European country :D')
+}
+neighbors[0] = 'Parliamentary Democracy of Canada';
 
+const myCountry = {
+    country: 'United States',
+    capital: 'Washington D.C.',
+    language: 'English',
+    population: 328,
+    neighbors: ['Canada', 'Mexico']
+};
+console.log(myCountry);
--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -188,7 +188,7 @@ console.log(friends.includes(23));
 if (friends.includes('Steven')) {
     console.log('You have a friend called Steven');
 }
-*/
+
 
 // Coding Challenge #2
 function calcTip(bill) {
@@ -201,3 +201,20 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 console.log(tips);
 const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(total);
+*/
+
+const jonasArray = [
+    'Jonas',
+    'Schedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+];
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
